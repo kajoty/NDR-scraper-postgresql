a88f03ca74fc1b0b1cf85b3a4660ce8be98de8b5
kajoty <karsten.jochens@gmail.com>
2025-04-03 13:56:55 +0200

    Update postgresql.py

--- functions/postgresql.py
+++ functions/postgresql.py
@@ -1,9 +1,11 @@
+# functions/postgresql.py
+
 import psycopg2
 from psycopg2.extras import execute_batch
 from datetime import datetime
 
-def initialize_postgresql(config):
-    return psycopg2.connect(
+def write_to_postgresql(config, data):
+    conn = psycopg2.connect(
         host=config["pg_host"],
         port=config["pg_port"],
         user=config["pg_user"],
@@ -11,49 +13,34 @@ def initialize_postgresql(config):
         dbname=config["pg_db"]
     )
 
-def write_to_postgresql(conn, data):
-    if not data:
-        return
+    query = """
+        INSERT INTO ndr_playlist (
+            checksum, station, artist, title,
+            played_date, played_time, played_at
+        )
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
+        ON CONFLICT (checksum, played_at) DO NOTHING;
+    """
 
-    try:
-        with conn.cursor() as cur:
-            query = """
-                INSERT INTO playlist_data (
-                    checksum, station, artist, title,
-                    played_date, played_time, played_at
-                )
-                VALUES (%s, %s, %s, %s, %s, %s, %s)
-                ON CONFLICT (checksum, played_at) DO NOTHING
-            """
+    records = []
 
-            values = []
-            for entry in data:
-                fields = entry["fields"]
-                tags = entry["tags"]
+    for entry in data:
+        fields = entry["fields"]
+        played_at = datetime.strptime(
+            f"{fields['played_date']} {fields['played_time']}", "%Y-%m-%d %H:%M:%S"
+        )
 
-                played_date = fields["played_date"]
-                played_time = fields["played_time"]
+        records.append((
+            fields['checksum'],
+            entry['tags']['station'],
+            fields['artist'],
+            fields['title'],
+            fields['played_date'],
+            fields['played_time'],
+            played_at
+        ))
 
-                try:
-                    played_at = datetime.strptime(
-                        f"{played_date} {played_time}", "%Y-%m-%d %H:%M"
-                    )
-                except ValueError:
-                    played_at = None
-
-                values.append((
-                    fields["checksum"],
-                    tags["station"],
-                    fields.get("artist"),
-                    fields.get("title"),
-                    played_date,
-                    played_time,
-                    played_at
-                ))
-
-            execute_batch(cur, query, values)
-        conn.commit()
-
-    except Exception as e:
-        conn.rollback()  # Fehlerhafte Transaktion zurücksetzen
-        print(f"[DB-Fehler] {e}")
+    with conn.cursor() as cur:
+        execute_batch(cur, query, records)
+    conn.commit()
+    conn.close()
